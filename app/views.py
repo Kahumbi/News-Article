bb5300620b822fe21d9108eacc6e1a262d6dd9dd
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-05-01 13:11:16 +0300

    Add a control block for title in index template

--- app/views.py
+++ app/views.py
@@ -2,6 +2,17 @@ from flask import render_template
 from app import app
 
 # Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to The best News Articles Website Online'
+    return render_template('index.html', title = title)
+
+
 @app.route('/news/<int:news_id>')
 def news(news_id):
 
